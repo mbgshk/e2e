159756cffdb2bad453a51e15da2117ed89804296
mbgshk <mbgshk@gmail.com>
2022-07-10 21:15:27 +0700

    Define Steps

--- login.cy.js
+++ login.cy.js
@@ -1,2 +1,23 @@
 /// <reference types="cypress" />
 
+describe('Login/Logout Test', () => {
+    before(() => {
+        // TODO
+    });
+
+    it('Should try to login with invalid data', () => {
+        // TODO
+    });
+
+    it('Should display error message', () => {
+        // TODO
+    });
+
+    it('Should login to application with valid data', () => {
+        // TODO
+    });
+
+    it('Should logout from the application', () => {
+        // TODO
+    });
+});
