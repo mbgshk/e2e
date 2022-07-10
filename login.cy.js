b23433dae2c0fe56b6f9be9196f4b94e4ab6a69f
mbgshk <mbgshk@gmail.com>
2022-07-10 21:27:08 +0700

    Code Negative Scenario

--- login.cy.js
+++ login.cy.js
@@ -2,15 +2,20 @@
 
 describe('Login/Logout Test', () => {
     before(() => {
-        // TODO
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.url().should('include', 'index.html')
+        cy.get('#signin_button').click()
     });
 
     it('Should try to login with invalid data', () => {
-        // TODO
+        cy.get('#login_form').should('be.visible')
+        cy.get('#user_login').type('invalid username')
+        cy.get('#user_password').type('invalid password')
+        cy.get('input[name="submit"]').click()
     });
 
     it('Should display error message', () => {
-        // TODO
+        cy.get('.alert-error').should('be.visible').and('contain', 'Login and/or password are wrong.')
     });
 
     it('Should login to application with valid data', () => {
